d42068cb17a2057716b08ce55ce70d93b3739ab5
Victor Rentea <victorrentea@gmail.com>
2020-09-15 02:35:06 +0300

    Cucumber (info.cukes) working + Junit Vintage

--- src/test/java/victor/testing/spring/repo/ProductSearchFeature.java
+++ src/test/java/victor/testing/spring/repo/ProductSearchFeature.java
@@ -1,11 +1,12 @@
 package victor.testing.spring.repo;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.platform.engine.Cucumber;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
-@Cucumber
-@CucumberOptions(features = "classpath:victor/testing/spring/repo/product-search.feature",
-        glue = {"victor.testing.spring.repo","victor.testing.spring.tools"})
+// Why still on JUnit4: https://github.com/cucumber/cucumber-jvm/issues/1149#issuecomment-611716745
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:product-search.feature",
+        glue = {"victor.testing.spring.repo","cucumber.api.spring"})
 public class ProductSearchFeature {
-
 }
--- src/test/java/victor/testing/spring/repo/ProductSearchSteps.java
+++ src/test/java/victor/testing/spring/repo/ProductSearchSteps.java
@@ -1,26 +1,20 @@
 package victor.testing.spring.repo;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.SomeSpringApplication;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.Product.Category;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.facade.ProductSearchCriteria;
 import victor.testing.spring.facade.ProductSearchResult;
-import victor.testing.spring.repo.ProductRepo;
-import victor.testing.spring.repo.SupplierRepo;
 
 import java.util.List;
 
@@ -28,72 +22,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @Slf4j
-@SpringBootTest
-@ActiveProfiles({"db-mem", "test"})
+@ContextConfiguration(
+    classes = SomeSpringApplication.class,
+    loader = SpringBootContextLoader.class)
+@ActiveProfiles({"db-mem","test"})
 public class ProductSearchSteps {
-    @Autowired
-    private ProductRepo productRepo;
-    @Autowired
-    private SupplierRepo supplierRepo;
+   @Autowired
+   private ProductRepo productRepo;
+   @Autowired
+   private SupplierRepo supplierRepo;
 
-    private ProductSearchCriteria criteria = new ProductSearchCriteria();
+   private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
-    private Product product;
+   private Product product;
 
-    @Given("^Supplier \"([^\"]*)\" exists$")
-    public void supplierExists(String supplierName) {
-        log.debug("Persisting supplier {}", supplierName);
-        supplierRepo.save(new Supplier().setName(supplierName));
-    }
+   @Given("^Supplier \"([^\"]*)\" exists$")
+   public void supplierExists(String supplierName) {
+      log.debug("Persisting supplier {}", supplierName);
+      supplierRepo.save(new Supplier().setName(supplierName));
+   }
 
-    @Given("^One product exists$")
-    public void aProductExists() {
-        product = new Product();
-    }
+   @Given("^One product exists$")
+   public void aProductExists() {
+      product = new Product();
+   }
 
-    @And("^That product has name \"([^\"]*)\"$")
-    public void thatProductHasName(String productName) {
-        product.setName(productName);
-    }
+   @And("^That product has name \"([^\"]*)\"$")
+   public void thatProductHasName(String productName) {
+      product.setName(productName);
+   }
 
-    @And("^That product has supplier \"([^\"]*)\"$")
-    public void thatProductHasSupplier(String supplierName) {
-        product.setSupplier(supplierRepo.findByName(supplierName));
-    }
+   @And("^That product has supplier \"([^\"]*)\"$")
+   public void thatProductHasSupplier(String supplierName) {
+      product.setSupplier(supplierRepo.findByName(supplierName));
+   }
 
-    @And("^That product has category \"([^\"]*)\"$")
-    public void thatProductHasCategory(Category category) {
-        product.setCategory(category);
-    }
+   @And("^That product has category \"([^\"]*)\"$")
+   public void thatProductHasCategory(Category category) {
+      product.setCategory(category);
+   }
 
 
-    @When("^The search criteria name is \"([^\"]*)\"$")
-    public void theSearchCriteriaNameIs(String productName) {
-        criteria.name = productName;
-    }
+   @When("^The search criteria name is \"([^\"]*)\"$")
+   public void theSearchCriteriaNameIs(String productName) {
+      criteria.name = productName;
+   }
 
-    @And("^The search criteria supplier is \"([^\"]*)\"$")
-    public void theSearchCriteriaSupplierIs(String supplierName) {
-        criteria.supplierId = supplierRepo.findByName(supplierName).getId();
-    }
+   @And("^The search criteria supplier is \"([^\"]*)\"$")
+   public void theSearchCriteriaSupplierIs(String supplierName) {
+      criteria.supplierId = supplierRepo.findByName(supplierName).getId();
+   }
 
-    @And("^The search criteria category is \"([^\"]*)\"$")
-    public void theSearchCriteriaCategoryIs(Category category) {
-        criteria.category = category;
-    }
+   @And("^The search criteria category is \"([^\"]*)\"$")
+   public void theSearchCriteriaCategoryIs(Category category) {
+      criteria.category = category;
+   }
 
-    @Then("^That product is returned by search$")
-    public void thatProductIsReturned() {
-        productRepo.save(product);
-        List<ProductSearchResult> results = productRepo.search(criteria);
-        assertThat(results).hasSize(1);
-        assertThat(results.get(0).id).isEqualTo(product.getId());
-    }
+   @Then("^That product is returned by search$")
+   public void thatProductIsReturned() {
+      productRepo.save(product);
+      List<ProductSearchResult> results = productRepo.search(criteria);
+      assertThat(results).hasSize(1);
+      assertThat(results.get(0).id).isEqualTo(product.getId());
+   }
 
-    @Then("^No products are returned by search$")
-    public void noProductsAreReturnedBySearch() {
-        productRepo.save(product);
-        List<ProductSearchResult> results = productRepo.search(criteria);
-        assertThat(results).isEmpty();
-    }
+   @Then("^No products are returned by search$")
+   public void noProductsAreReturnedBySearch() {
+      productRepo.save(product);
+      List<ProductSearchResult> results = productRepo.search(criteria);
+      assertThat(results).isEmpty();
+   }
 }
