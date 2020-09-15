86b5a047d6906f0bf739d0b8f5438ddc8babca64
Victor Rentea <victorrentea@gmail.com>
2020-09-15 18:26:59 +0300

    Test can target a running Spring app

---
+++ src/test/java/victor/testing/spring/web/ProductBlackTest.java
@@ -0,0 +1,57 @@
+package victor.testing.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import victor.testing.spring.domain.Supplier;
+import victor.testing.spring.infra.SafetyServiceClient;
+import victor.testing.spring.repo.SupplierRepo;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductBlackTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private SafetyServiceClient safetyClient;
+    @Autowired
+    private SupplierRepo supplierRepo;
+
+    @Test
+    public void testSearch() throws Exception {
+        // given
+        Long supplierId = supplierRepo.save(new Supplier().setActive(true)).getId();
+        when(safetyClient.isSafe("UPC")).thenReturn(true);
+
+        // when create
+        // language=json
+        String createJson = String.format("{\"name\": \"Tree\", \"supplierId\": \"%d\", \"upc\": \"UPC\"}", supplierId);
+        mockMvc.perform(post("/product/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+            .andExpect(status().isOk());
+
+        // then search
+        mockMvc.perform(post("/product/search")
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(header().string("Custom-Header", "true"))
+                .andExpect(jsonPath("$[0].name").value("Tree"));
+    }
+
+
+}
---
+++ src/test/java/victor/testing/spring/web/ProductRestTest.java
@@ -0,0 +1,71 @@
+package victor.testing.spring.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import victor.testing.spring.domain.ProductCategory;
+import victor.testing.spring.domain.Supplier;
+import victor.testing.spring.facade.ProductSearchCriteria;
+import victor.testing.spring.facade.ProductSearchResult;
+import victor.testing.spring.infra.SafetyServiceClient;
+import victor.testing.spring.repo.SupplierRepo;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductRestTest {
+   @MockBean
+   private SafetyServiceClient safetyClient;
+   @Autowired
+   private SupplierRepo supplierRepo;
+//    @Autowired
+//    private TestRestTemplate rest; // vs RestTemplate + URL + .withBasicAuth("spring", "secret")
+
+   @Value("http://localhost:8080")
+   private String baseUri;
+
+   @Test
+   public void testSearch() throws Exception {
+      RestTemplate rest = new RestTemplate();
+
+      Long supplierId = supplierRepo.save(new Supplier().setActive(true)).getId();
+      when(safetyClient.isSafe("UPC")).thenReturn(true);
+
+      var productDto = new ProductDto("Tree", "UPC", supplierId, ProductCategory.ME);
+      var createResult = rest.postForEntity(baseUri + "/product/create", productDto, Void.class);
+      assertEquals(HttpStatus.OK, createResult.getStatusCode());
+
+      var searchCriteria = new ProductSearchCriteria("Tree", null, null);
+      ResponseEntity<List<ProductSearchResult>> searchResponse = rest.exchange(
+          baseUri + "/product/search", HttpMethod.POST,
+          new HttpEntity<>(searchCriteria), new ParameterizedTypeReference<>() {
+      });
+
+      assertEquals(HttpStatus.OK, searchResponse.getStatusCode());
+      assertThat(searchResponse.getBody()).allMatch(p -> "Tree".equals(p.getName()));
+   }
+
+
+}
