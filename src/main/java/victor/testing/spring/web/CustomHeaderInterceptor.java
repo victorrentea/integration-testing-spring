d46de03d5f9784a7b764517787ce4a6f06e7ca59
Victor Rentea <victorrentea@gmail.com>
2020-09-16 13:44:13 +0300

    explanations

--- src/main/java/victor/testing/spring/web/CustomHeaderInterceptor.java
+++ src/main/java/victor/testing/spring/web/CustomHeaderInterceptor.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CustomHeaderInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.debug("Interceptor Runs");
+        log.debug("HTTP Interceptor adding custom header to response");
         response.addHeader("Custom-Header", "true");
         return true;
     }
