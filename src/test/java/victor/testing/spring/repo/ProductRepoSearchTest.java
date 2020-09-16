0f9a06432f9a6cb864a1fe757d217cf2774723ab
Victor Rentea <victorrentea@gmail.com>
2020-09-16 14:59:41 +0300

    Every @Test will run in its own @Transaction, rolledback at the end of the test. Includes @Before...

--- src/test/java/victor/testing/spring/repo/ProductRepoSearchTest.java
+++ src/test/java/victor/testing/spring/repo/ProductRepoSearchTest.java
@@ -1,14 +1,17 @@
 package victor.testing.spring.repo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.facade.ProductSearchCriteria;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 @ActiveProfiles("db-mem")
 public class ProductRepoSearchTest {
@@ -17,9 +20,18 @@ public class ProductRepoSearchTest {
 
     private ProductSearchCriteria criteria = new ProductSearchCriteria();
 
+    @BeforeEach
+    public void initialize() {
+        repo.save(new Product());
+
+    }
     @Test
     public void noCriteria() {
-        repo.save(new Product());
+        assertThat(repo.search(criteria)).hasSize(1);
+    }
+    @Test
+    public void noCriteri2a() {
+//        repo.save(new Product());
         assertThat(repo.search(criteria)).hasSize(1);
     }
 
