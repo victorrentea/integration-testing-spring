6f8de8add577d1b1c89cc88d2b7f72c544d730af
Victor Rentea <victorrentea@gmail.com>
2020-09-16 15:31:22 +0300

    Delay the startup of your Spring Context from tests until the database is up

--- src/test/java/victor/testing/spring/repo/ProductRepoSearchTest.java
+++ src/test/java/victor/testing/spring/repo/ProductRepoSearchTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import victor.testing.spring.WaitForDatabase;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.domain.Supplier;
 import victor.testing.spring.facade.ProductSearchCriteria;
@@ -19,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("db-mysql")
 @Tag("integration")
+@ContextConfiguration(initializers = WaitForDatabase.class)
 public class ProductRepoSearchTest {
     @Autowired
     private ProductRepo repo;
