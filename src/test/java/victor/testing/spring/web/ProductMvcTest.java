8de4b8d7aa1d8b4b40f30ff546fa51d4ebdcd64f
Victor Rentea <victorrentea@gmail.com>
2020-09-16 17:20:27 +0300

    Tests fail becuase data is left in DB (COMMIT)

--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -7,13 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.repo.ProductRepo;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("db-mem")
@@ -34,6 +37,27 @@ public class ProductMvcTest {
         )
             .andExpect(status().isOk())
             .andExpect(header().string("Custom-Header", "true"))
+            .andExpect(jsonPath("$", hasSize(1)));
+//            .andExpect(jsonPath("$[0].name").value("Tree"));
+    }
+
+    // The MockMvc EMULATES a HTTP call, w/o Tomcat, w/o any HTTP Worker Thread Pool,
+    // I'm running the COntroller in the same thread as the test
+    // and since the Transaction in Spring is bound to the current thread ->
+    // I can make sure the .search repo works in the same Tx as the test one,
+    // in which I INSERTed the Product
+
+    @Test
+    public void testSearc2h() throws Exception {
+        productRepo.save(new Product().setName("Tree"));
+
+        mockMvc.perform(post("/product/search")
+            .content("{}")
+            .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk())
+            .andExpect(header().string("Custom-Header", "true"))
+
 
             .andExpect(jsonPath("$[0].name").value("Tree"));
     }
