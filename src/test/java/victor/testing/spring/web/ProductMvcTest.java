5f0f856db168eb342ff14598bde7afb84077475b
Victor Rentea <victorrentea@gmail.com>
2020-09-16 17:14:12 +0300

    Proud commit: testing SEARCH ALL THE WAY From JSON to DB

--- src/test/java/victor/testing/spring/web/ProductMvcTest.java
+++ src/test/java/victor/testing/spring/web/ProductMvcTest.java
@@ -5,19 +5,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import victor.testing.spring.domain.Product;
 import victor.testing.spring.repo.ProductRepo;
 
+import static org.mockito.ArgumentMatchers.contains;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("db-mem")
 public class ProductMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepo productRepo;
 
     @Test
     public void testSearch() throws Exception {
-       // TODO
+        productRepo.save(new Product().setName("Tree"));
+
+        mockMvc.perform(post("/product/search")
+            .content("{}")
+            .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk())
+            .andExpect(header().string("Custom-Header", "true"))
+
+            .andExpect(jsonPath("$[0].name").value("Tree"));
     }
 
 
